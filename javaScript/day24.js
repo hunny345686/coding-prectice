ab764f54511fefa3676fb5021dc8be22efadb453
Prem Singh <hunny345686@gmail.com>
2026-09-20 13:47:52 +0530

    coding Hosting concept how hosting works in js

--- javaScript/day24.js
+++ javaScript/day24.js
@@ -12,9 +12,9 @@ function createFunctions() {
 
 const funcs = createFunctions();
 
-console.log(funcs[0]());
-console.log(funcs[1]());
-console.log(funcs[2]());
+// console.log(funcs[0]());
+// console.log(funcs[1]());
+// console.log(funcs[2]());
 
 // 0 1 2
 
@@ -31,9 +31,54 @@ function outer() {
 const a = outer();
 const b = outer();
 
-console.log(a());
-console.log(a());
-console.log(b());
-console.log(b());
+// console.log(a());
+// console.log(a());
+// console.log(b());
+// console.log(b());
 
 // 1 2 1 2
+
+// console.log(a); // undifined
+
+// var a = 10;
+
+// console.log(a);
+
+// undifined 10
+
+//  Task 3
+
+console.log(a);
+
+var a = 10;
+
+function test() {
+  console.log(a);
+  var a = 20;
+}
+
+test();
+
+//  undifiend , undifiend
+
+let a = 10;
+
+{
+  console.log(a);
+  let a = 20;
+}
+
+// give a reffrence eroor
+
+sayHello();
+
+var sayHello = function () {
+  console.log("Hello");
+};
+
+function sayHello() {
+  console.log("Hi");
+}
+
+// when we call var sayhello it will give type error that sayhello is not a function
+// and second will console HI
