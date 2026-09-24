a0ca58c88b69f985398b0ea598fd543f2c1c9876
Prem Singh <hunny345686@gmail.com>
2026-09-24 18:55:46 +0530

    js coding prectice - prototype chain lookup, inherited properties, own properties, and property shadowing

--- coding-inteview/js/round5.1.js
+++ coding-inteview/js/round5.1.js
@@ -25,3 +25,47 @@ const p3 = Promise.resolve(30);
 Promise.myAll([p1, p2, p3]).then((result) => {
   console.log(result);
 });
+
+// Prototype Chain & Property Lookup
+
+const animal = {
+  eats: true,
+};
+
+const dog = Object.create(animal);
+
+dog.barks = true;
+
+// console.log(dog.barks);
+// console.log(dog.eats);
+console.log(dog.hasOwnProperty("barks")); // true
+console.log(dog.hasOwnProperty("eats")); // false
+
+const animal1 = {
+  sound: "generic",
+};
+
+const dog1 = Object.create(animal1);
+
+dog1.sound = "bark";
+
+console.log(dog1.sound);
+console.log(animal1.sound);
+
+// A tricky interview question
+
+const parent = {
+  name: "Parent",
+  age: 50,
+};
+
+const child = Object.create(parent);
+
+child.name = "Child";
+
+console.log(child.name);
+console.log(child.age);
+console.log(child.hasOwnProperty("name"));
+console.log(child.hasOwnProperty("age"));
+
+// Child , 50 true, false
